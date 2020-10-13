d67b43f1647c52ef5587bd33f96ad039262f7e54
NIcolasBonamy <n.bonamy@live.fr>
2020-10-13 12:30:08 +0200

    add ContactList using map

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import './Contact.css';
+import PropTypes from 'prop-types';
 
-function Contact() {
+function Contact(props) {
     return (
         <div className="Contact">
-            <img className="avatar" src="https://randomuser.me/api/portraits/men/3.jpg" alt="Wallace Kim"/>
+            <img className="avatar" src={props.avatar} alt="Wallace Kim"/>
             <div>
-                <p className="name">Wallace Kim</p>               
+                <p className="name">{props.name}</p>               
                     <div className="status">
-                        <div className="status-online" />
-                        <p className="status-text">online</p>
+                        <div className={props.online ? "status-online" : "status-offline"} />
+                        <p className="status-text">{props.statustext}</p>
                     </div>                
             </div>
         </div>
     );
 }
+
+Contact.propTypes = {
+    name: PropTypes.string.isRequired,
+    statustext: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    online: PropTypes.bool,
+  };
 
 export default Contact;
